8f50f4b5b7a5bdb5bbe4e6be4be467cf44020dd4
Robert Vásquez <vasquezrober09@gmail.com>
2023-02-14 12:13:15 -0430

    create script for calculate tip

---
+++ script.js
@@ -0,0 +1,21 @@
+const sliders = document.querySelectorAll("input[type='range']");
+sliders.forEach(function(slider){
+	slider.addEventListener("input", calculateTip)
+});
+
+const billInput = document.getElementById("bill");
+billInput.addEventListener("change",calculateTip);
+
+function calculateTip(){
+	let bill = parseFloat(billInput.value);
+	let tipPercent = document.getElementById("tip").value;
+	let noOfPeople = document.getElementById("no-of-people").value;
+
+	billInput.value = bill.toFixed(2);
+
+	let totalTip = parseFloat((bill * (tipPercent/100)).toFixed(2));
+	let total = parseFloat((bill + totalTip).toFixed(2));
+
+	let tipPerPerson = (totalTip / noOfPeople).toFixed(2);
+	let totalPerPerson = (total / noOfPeople).toFixed(2);
+}
